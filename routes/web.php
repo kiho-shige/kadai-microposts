c55fb589fd4aa64de5ac92798a8faedefe941de1
Kiho Shigematsu <kiho-shigematsu@dac.co.jp>
2023-06-06 06:39:22 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
-use App\Http\Controllers\MicropostsController; //追記
+use App\Http\Controllers\MicropostsController; 
+use App\Http\Controllers\UserFollowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +22,14 @@ Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['a
 
 require __DIR__.'/auth.php';
 
-Route::group(['middleware' => ['auth']], function () {                                   
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'users/{id}'], function () {                                          
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');     
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow'); 
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings'); 
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');    
+    });                                                                                             
+    
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });
